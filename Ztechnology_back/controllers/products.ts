2675ebff1d77094a64ce63ac0cb56c10f749088c
pablodlaroza <pablodlarosaa@gmail.com>
2023-06-30 11:02:49 -0500

    6/30/2023

--- Ztechnology_back/controllers/products.ts
+++ Ztechnology_back/controllers/products.ts
@@ -1,2 +1,60 @@
 import { Request, Response } from "express";
-import Products from "../models/products";
+import Products from "../models/products"
+
+export const consultProducts = async (req: Request, res: Response) => {
+    const products = await Products.findAll();
+
+    res.status(200).json({
+        msg:'Clientes',
+        products
+    })
+    
+}
+
+export const saveProducts = async(req: Request, res: Response) => {
+
+    const {name, description, price} = req.body;
+    
+
+    console.log('Producto Registrado:', name);
+
+    const products = await Products.create({
+        name, description, price
+    })
+    
+
+    res.status(200).json({
+        msg: `Se ha registrado un Producto con el id: ${products.dataValues.id}`
+    })
+   
+}
+
+export const updateProduct = async(req: Request, res: Response) => {
+    const {id, name, description, price} = req.body;
+    console.log(req.body)
+
+    const products = await Products.update({ name, description, price},{
+        where:{
+            id
+        }
+    })
+
+    res.status(200).json({
+        msg: `el producto con el id ${id} ha sido actualizado`
+    })
+}
+
+export const deleteProduct = async(req: Request, res: Response) => {
+    const {id} = req.params;
+    console.log(req.body)
+
+    await Products.destroy({
+        where:{
+            id
+        }
+    })
+
+    res.status(200).json({
+        msg: `el producto con el id ${id} ha sido eliminado`
+    })
+}
--- Ztechnology_back/routes/products.ts
+++ Ztechnology_back/routes/products.ts
@@ -1,2 +1,11 @@
 import { Router } from "express";
-import {}
+import { consultProducts, saveProducts,updateProduct,deleteProduct } from "../controllers/products";
+
+const router = Router()
+
+router.get('/consultProducts', consultProducts)
+router.post('/saveProducts', saveProducts)
+router.put('/updateProduct', updateProduct)
+router.delete('/deleteProduct/:id', deleteProduct)
+
+export default router
